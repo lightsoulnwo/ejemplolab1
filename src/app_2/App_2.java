f7658050eabc259c53795e21c9ba34265e4dc541
JP VEGA <the-k@DESKTOP-TIA7P1E>
2020-05-08 23:47:52 -0500

    pregunta 2

--- src/app_2/App_2.java
+++ src/app_2/App_2.java
@@ -5,6 +5,8 @@
  */
 package app_2;
 
+import java.io.File;
+import java.util.Scanner;
 import lab1.Menu;
 
 /**
@@ -12,16 +14,39 @@ import lab1.Menu;
  * @author the-k
  */
 public class App_2 extends Menu{
-
+  Scanner scanner = new Scanner(System.in);
     @Override
     public void mostrarMenu() {
         
+        Personas personas = objectMapper.readValue(new File("C:\\Users\\the-k\\Desktop\\ttttt\\ejemplolab1\\src\\lab1\\personas.json"), personas.class);
+         int entrada = 0;
+
+        do {
+            System.out.println("MENU DE APP_2");
+            System.out.println("INGRESE UNA OPCION");
+
+            System.out.println("1.-LISTAR=MOSTRAREL NUMER DE ORDEN DE TODAS LAS PERSONAS");
+            System.out.println("2.-CREAR UNA PERSONA CON TODOS LOSCAMPOS");
+            System.out.println("3.-ELIMINAR A UNA PERSONA");
+            System.out.println("4.-GUARDAR UNA NUEVA LISTA");
+            System.out.println("5.- SALIR");
+            entrada = scanner.nextInt();
+            while (entrada != 1 && entrada != 2 && entrada != 3&& entrada != 5&& entrada != 4) {
+                System.out.println("INGRESA UN OPCION VALIDA");
+                entrada = scanner.nextInt();
+
+            }
+            if(entrada==1||entrada==2||entrada==3||entrada==4){
+                Accion(entrada);
+            }
+          
+        } while (entrada != 3);
         
         }
 
     @Override
     public void Accion(int acc) {
-        SADFADSFADSFASF;
+     
        }
     
 }
---
+++ src/app_2/Personas.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_2;
+
+/**
+ *
+ * @author the-k
+ */
+public class Personas {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String genero;
+    private String ciudad;
+    private String fechaNac;
+    private Double peso;
+    private String ip;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the ciudad
+     */
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    /**
+     * @param ciudad the ciudad to set
+     */
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    /**
+     * @return the fechaNac
+     */
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    /**
+     * @param fechaNac the fechaNac to set
+     */
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    /**
+     * @return the peso
+     */
+    public Double getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the ip
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * @param ip the ip to set
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    
+    
+    
+}
